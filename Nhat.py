f43f9247cb92e4dab7d8504dbf73571c44042b97
minhnhat416 <nhatnhm20416c@st.uel.edu.vn>
2022-03-30 17:37:04 +0700

    canvas widget

---
+++ Nhat.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+#Canvas
+root = Tk()
+root.title('Codemy.com - Canvas')
+# root.geometry('500x500')
+my_canvas = Canvas(width=500, height=500,bg = 'white')
+# my_canvas.pack(pady=20)
+
+root.mainloop()

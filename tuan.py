510a48b15ad717a94cf2c93673d618619ff9ec24
LE THANH TUAN <tuanlt21416c@st.uel.edu.vn>
2022-04-04 12:29:05 +0700

    code vd Message

--- tuan.py
+++ tuan.py
@@ -1,7 +1,17 @@
-#Toplevel
+#Message
 from tkinter import *
-root = Tk()
-root.title('GfG')
-top = Toplevel()
-top.title('Python')
-top.mainloop()
+main = Tk()
+ourMessage ='This is our Message'
+messageVar = Message(main, text = ourMessage)
+messageVar.config(bg='lightgreen')
+messageVar.pack( )
+main.mainloop( )
+
+from tkinter import *
+master = Tk()
+w = Scale(master, from_=0, to=42)
+w.pack()
+w = Scale(master, from_=0, to=200, orient=HORIZONTAL)
+w.pack()
+mainloop()
+

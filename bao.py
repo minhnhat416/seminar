a59fad87c1227f0a94eb343705ad49f1b548da7e
010011101 <baonc21416c@st.uel.edu.vn>
2022-04-04 10:51:52 +0700

    code button

--- bao.py
+++ bao.py
@@ -1,2 +1,6 @@
-print('Bao ne')
-print(" Phú nè")
+import tkinter as tk
+r = tk.Tk()
+r.title('Counting Seconds')
+button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+button.pack()
+r.mainloop()

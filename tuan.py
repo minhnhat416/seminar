577f36401a4667f0fa6e4131aa3d50f976fa7994
LE THANH TUAN <tuanlt21416c@st.uel.edu.vn>
2022-04-04 12:31:27 +0700

    Scalee

--- tuan.py
+++ tuan.py
@@ -1,12 +1,4 @@
-#Message
-from tkinter import *
-main = Tk()
-ourMessage ='This is our Message'
-messageVar = Message(main, text = ourMessage)
-messageVar.config(bg='lightgreen')
-messageVar.pack( )
-main.mainloop( )
-
+#Scale
 from tkinter import *
 master = Tk()
 w = Scale(master, from_=0, to=42)
@@ -14,4 +6,3 @@ w.pack()
 w = Scale(master, from_=0, to=200, orient=HORIZONTAL)
 w.pack()
 mainloop()
-

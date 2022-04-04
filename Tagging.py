c2d2fbe77f99b137226e59f3934a37eaddd9ad3c
minhnhat416 <nhatnhm20416c@st.uel.edu.vn>
2022-04-04 09:04:20 +0700

    Ví dụ vè tagging

--- Tagging.py
+++ Tagging.py
@@ -1,22 +1,25 @@
-import tkinter as tk
+from tkinter import *
 
-my_w = tk.Tk()
+# Tạo cửa sổ
+my_w = Tk()
 my_w.geometry("400x300")
-l1 = tk.Label(my_w,text='Your Name', width=10) #added one Label
+
+#Thêm một label
+l1 = Label(my_w,text='Your Name', width=10)
 l1.grid(row=1,column=1)
 
-t1 = tk.Text(my_w,width=35,height=4) #text box
+#Thêm một text box
+t1 = Text(my_w,width=35,height=4)
 t1.grid(row=1,column=2,columnspan=2,pady=30)
-
 font1=('Times',16,'underline')
-t1.insert(tk.END, "Welcome to plus2net\n Hello word")
+t1.insert(END, "Hello word")
 
-# tag name my_hg is created
+# Tạo tag_name 'my_hg'
 t1.tag_add("my_hg", "1.0", "1.9")
 t1.tag_config("my_hg",background="yellow",foreground="red",font=font1)
 
-#t1.tag_delete('my_hg')
-b1=tk.Button(my_w,text='Remove tag',command=lambda :t1.tag_remove('my_hg','1.0','1.9'))
+# xóa tag_name('my_hg') bằng button
+b1=Button(my_w,text='Remove tag',command=lambda :t1.tag_remove('my_hg','1.0','1.9'))
 b1.grid(row=2,column=2,pady=10)
 
 

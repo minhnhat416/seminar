38eef3e30cd26614fac895071861a9fd67abb6fc
010011101 <baonc21416c@st.uel.edu.vn>
2022-04-04 11:14:43 +0700

    code menu

--- bao.py
+++ bao.py
@@ -1,6 +1,15 @@
-import tkinter as tk
-r = tk.Tk()
-r.title('Counting Seconds')
-button = tk.Button(r, text='Stop', width=25, command=r.destroy)
-button.pack()
-r.mainloop()
+from tkinter import *
+
+root = Tk()
+menu = Menu(root)
+root.config(menu=menu)
+filemenu = Menu(menu)
+menu.add_cascade(label='File', menu=filemenu)
+filemenu.add_command(label='New')
+filemenu.add_command(label='Open...')
+filemenu.add_separator()
+filemenu.add_command(label='Exit', command=root.quit)
+helpmenu = Menu(menu)
+menu.add_cascade(label='Help', menu=helpmenu)
+helpmenu.add_command(label='About')
+mainloop()
